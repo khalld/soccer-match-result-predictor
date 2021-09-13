d497f2b6f4b4c24e7cee0b0b6c997d5f9cd6eb67
khalld <37438774+khalld@users.noreply.github.com>
2021-09-13 19:01:40 +0200

    update

--- libs/utils.py
+++ libs/utils.py
@@ -1,4 +1,5 @@
 from numba import jit # import decorator that allow to use gpu
+from timeit import default_timer as timer   
 
 @jit
 def find_penalty(row, sht_csv, sht_csv_len):
@@ -44,3 +45,17 @@ def add_labels(elem, serie):
     
     if elem == serie['75%']:
         return 'perc_75%'
+
+# @jit(nopython=True)
+def find_range(year):
+    arr = ['1870-1879', '1880-1889', '1890-1899', '1900-1909', '1910-1919', '1920-1929', '1930-1939', '1940-1949', '1950-1959', '1960-1969', '1970-1979', '1980-1989', '1990-1999', '2000-2009', '2010-2020','2021-2029']
+
+    for i in arr:
+        # print(i)
+        x = int(i[:4])
+        y = int(i[5:9]) + 1
+
+        range_year = range(x, y)
+
+        if year in range_year:
+            return str(i)
