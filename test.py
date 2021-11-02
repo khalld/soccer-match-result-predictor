288f941e7bf2dda8bca29a3407d9189ea05e3d34
Danilo Leocata <danilo.leocata@gmail.com>
2021-11-02 22:45:28 +0100

    update

--- test.py
+++ test.py
@@ -18,4 +18,32 @@ plt.rcParams.update({'figure.figsize':(45,10), 'figure.dpi':100})
 PATH_ORIGINAL_DST = 'dataset/original'
 PATH_DST = 'dataset'
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    df = pd.read_csv(path.join(PATH_DST, 'dataset_v2_continent.csv')).drop(columns=['Unnamed: 0'])
+    # print(df.columns)
+
+    valid_country = pd.read_csv(path.join(PATH_DST, 'dataset_v1_valid_country.csv')).drop(columns=['Unnamed: 0']).team.values
+
+    df_country = df.country.drop_duplicates().values
+
+    # print(df_country)
+
+    not_valid_country = []
+
+    for i in df_country:
+        if i not in valid_country:
+            not_valid_country.append(i)
+        
+    # print(not_valid_country)
+
+    # trovati altri record non validi. Li elimino
+
+    print("Lunghezza del dataframe prima del controllo: %s" % (len(df)) )
+
+    for i, row in df.iterrows():
+        if(row.country in not_valid_country):
+            df.drop(i, inplace=True)
+
+    print("Lunghezza del dataframe prima del controllo: %s" % (len(df)) )
+
+    df = df.reset_index(drop=True).to_csv(path.join(PATH_DST, 'dataset_v3.csv'))
