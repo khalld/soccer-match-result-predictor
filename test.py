537cc4f4d869dda106ce72f73bedda9c03767acf
Danilo Leocata <danilo.leocata@gmail.com>
2021-11-02 23:48:23 +0100

    aggiunte funzione estrazione feature

--- test.py
+++ test.py
@@ -19,37 +19,6 @@ PATH_ORIGINAL_DST = 'dataset/original'
 PATH_DST = 'dataset'
 
 if __name__ == "__main__":
-    df = pd.read_csv(path.join(PATH_DST, 'dataset_v3.csv')).drop(columns=['Unnamed: 0'])
-
-    # mi calcolo le 
-
-    teams = pd.read_csv(path.join(PATH_DST, 'dataset_v1_valid_country.csv')).drop(columns=['Unnamed: 0'])
-
-    teams['games'] = 0
-    teams['home_games'] = 0
-    teams['away_games'] = 0
-    teams['neutral_games'] = 0
-
-    teams['wins'] = 0
-    teams['home_wins'] = 0
-    teams['away_wins'] = 0
-    teams['neutral_wins'] = 0
-    
-    teams['home_defeats'] = 0
-    teams['away_defeats'] = 0
-    teams['neutral_defeats'] = 0
-
-    teams['home_draws'] = 0
-    teams['away_draws'] = 0
-    teams['neutral_draws'] = 0
-
-    teams['home_goals_scored'] = 0
-    teams['away_goals_scored'] = 0
-    teams['neutral_draws_scored'] = 0
-
-    teams['home_goals_conceded'] = 0
-    teams['away_goals_conceded'] = 0
-    teams['neutral_goals_conceded'] = 0
-
-
-    teams.to_csv(path.join(PATH_DST, 'dataset_v4.csv'))
+    # creo un dataframe con le statistiche di ogni squadra per vedere se ci sono delle correlazioni sul giocare in casa o no nello specifico
+    # e nei goal subiti/segnati
+    df = pd.read_csv(path.join(PATH_DST, 'teams_statistics.csv'))
