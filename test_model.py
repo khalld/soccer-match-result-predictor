4c9673c706bed81a0e591f3645cb8e81e893a958
Danilo Leocata <danilo.leocata@gmail.com>
2021-11-06 21:45:16 +0100

    update

--- test_model.py
+++ test_model.py
@@ -29,16 +29,82 @@ plt.rcParams.update({'figure.figsize':(10,10), 'figure.dpi':100})
 PATH_ORIGINAL_DST = 'dataset/original'
 PATH_DST = 'dataset'
 
+def test_Linear_regr(df):
+    print("***"*5 + " FIRST ITERATION " + "***"*5)
+    formula = "outcome ~ home_team + away_team + tournament + city + country + continent + neutral"
+    model = ols(formula=formula, data=df).fit()
+    print(model.summary())
+
+    # il regressore non è significativo perché presenta un R-Squared basso.
+    # i p-value di neutral e continent sono uguali a 0 quindi significa che sono statisticamente rilevanti
+    # procedendo con la backward notiamo che il valore con p value più alto è tournament quindi lo elimino e procedo nuovamente
+
+    print("***"*5 + " SECOND ITERATION " + "***"*5)
+    formula = "outcome ~ home_team + away_team + city + country + continent + neutral"
+    model = ols(formula=formula, data=df).fit()
+    print(model.summary())
+
+    # è aumentata la F-statistic ma non la R-squaded.
+    # inoltre prob-f-statistic non è ancora nullo quindi non è statisticamente significativo
+    # Continuo elemininando la variabile con p-value maggiore che è country
+
+    print("***"*5 + " THIRD ITERATION " + "***"*5)
+    formula = "outcome ~ home_team + away_team + city + continent + neutral"
+    model = ols(formula=formula, data=df).fit()
+    print(model.summary())
+
+    # idem di sopra continuo eliminando city
+    # -------- modello migliore con regressione lineare
+    print("***"*5 + " FOURTH ITERATION " + "***"*5)
+    formula = "outcome ~ home_team + away_team + continent + neutral"
+    model = ols(formula=formula, data=df, ).fit()
+    print(model.summary())
+
+    # aggiungere un peso ad ogni riga del dal dataset non ha fatto migliorare per nulla l'algoritmo
+    # neanche applicare lo z-score ai goals
+
+def test_Poisson(df):
+    # ------- inizi test per poisson
+    print("***"*5 + " POISSON first ITERATION " + "***"*5)
+    formula = "outcome ~ home_team + away_team + tournament + city + country + continent + neutral"
+    model = smf.glm(formula=formula, data=df, family=sm.families.Poisson()).fit() # freq_weights, ricorda eventualmente di considerare..
+    print(model.summary())
+
+    # elimino tournament che è quello col p-value + alto
+    print("***"*5 + " POISSON second ITERATION " + "***"*5)
+    formula = "outcome ~ home_team + away_team + city + country + continent + neutral"
+    model = smf.glm(formula=formula, data=df, family=sm.families.Poisson()).fit() # freq_weights, ricorda eventualmente di considerare..
+    print(model.summary())
+
+    # elimino country ...
+    print("***"*5 + " POISSON third ITERATION " + "***"*5)
+    formula = "outcome ~ home_team + away_team + city + continent + neutral"
+    model = smf.glm(formula=formula, data=df, family=sm.families.Poisson()).fit() # freq_weights, ricorda eventualmente di considerare..
+    print(model.summary())
+
+    # elimino city
+    print("***"*5 + " POISSON fourth ITERATION " + "***"*5)
+    formula = "outcome ~ home_team + away_team + continent + neutral"
+    model = smf.glm(formula=formula, data=df, family=sm.families.Poisson()).fit() # freq_weights, ricorda eventualmente di considerare..
+    print(model.summary())
+
+def test_Logit(df):
+    #TODO
+
+    print("***"*5 + " LOGIT FIRST ITERATION " + "***"*5)
+    formula = "outcome ~ home_team + away_team + tournament + city + country + continent + neutral"
+    model = logit(formula=formula, data=df).fit() # freq_weights, ricorda eventualmente di considerare..
+    print(model.summary())
+
+
 
 if __name__ == "__main__":
 
     # parto da questo dataset nel notebook
     # df = pd.read_csv(path.join(PATH_DST, 'dataset_v3.csv')).drop(columns=['Unnamed: 0'])
-    # LABEL ENCODING
+    # print("***"*5 + "STARTING LABEL ENCODING PROCESS" + "***"*5)
     # df = label_encoding(df)
-
     # df.drop(columns=['year']).to_csv(path.join(PATH_DST, 'dataset_v3_ENCODED.csv'))
-
     # print("***"*5 + "ENDED LABEL ENCODING PROCESS" + "***"*5)
 
     df = pd.read_csv(path.join(PATH_DST, 'dataset_v3_ENCODED.csv')).drop(columns=['Unnamed: 0'])
@@ -46,61 +112,6 @@ if __name__ == "__main__":
     # print(df.corr())
     # print(df.columns)
 
-    # print("***"*5 + " FIRST ITERATION " + "***"*5)
-    # formula = "outcome ~ home_team + away_team + tournament + city + country + continent + neutral"
-    # model = ols(formula=formula, data=df).fit()
-    # print(model.summary())
-
-    # il regressore non è significativo perché presenta un R-Squared basso.
-
-    # i p-value di neutral e continent sono uguali a 0 quindi significa che sono statisticamente rilevanti
-
-    # procedendo con la backward notiamo che il valore con p value più alto è tournament quindi lo elimino e procedo nuovamente
-
-    # print("***"*5 + " SECOND ITERATION " + "***"*5)
-    # formula = "outcome ~ home_team + away_team + city + country + continent + neutral"
-    # model = ols(formula=formula, data=df).fit()
-    # print(model.summary())
-
-    # è aumentata la F-statistic ma non la R-squaded.
-    # inoltre prob-f-statistic non è ancora nullo quindi non è statisticamente significativo
-    # Continuo elemininando la variabile con p-value maggiore che è country
-
-    # print("***"*5 + " THIRD ITERATION " + "***"*5)
-    # formula = "outcome ~ home_team + away_team + city + continent + neutral"
-    # model = ols(formula=formula, data=df).fit()
-    # print(model.summary())
-
-    # idem di sopra continuo eliminando city
-    # -------- modello migliore con regressione lineare
-    # print("***"*5 + " FOURTH ITERATION " + "***"*5)
-    # formula = "outcome ~ home_team + away_team + continent + neutral"
-    # model = ols(formula=formula, data=df, ).fit()
-    # print(model.summary())
-
-    # aggiungere un peso ad ogni riga del dal dataset non ha fatto migliorare per nulla l'algoritmo
-    # neanche applicare lo z-score ai goals
-
-    # ------- inizi test per poisson
-    # print("***"*5 + " POISSON first ITERATION " + "***"*5)
-    # formula = "outcome ~ home_team + away_team + tournament + city + country + continent + neutral"
-    # model = smf.glm(formula=formula, data=df, family=sm.families.Poisson()).fit() # freq_weights, ricorda eventualmente di considerare..
-    # print(model.summary())
-
-    # elimino tournament che è quello col p-value + alto
-    # print("***"*5 + " POISSON second ITERATION " + "***"*5)
-    # formula = "outcome ~ home_team + away_team + city + country + continent + neutral"
-    # model = smf.glm(formula=formula, data=df, family=sm.families.Poisson()).fit() # freq_weights, ricorda eventualmente di considerare..
-    # print(model.summary())
-
-    # elimino country ...
-    # print("***"*5 + " POISSON third ITERATION " + "***"*5)
-    # formula = "outcome ~ home_team + away_team + city + continent + neutral"
-    # model = smf.glm(formula=formula, data=df, family=sm.families.Poisson()).fit() # freq_weights, ricorda eventualmente di considerare..
-    # print(model.summary())
-
-    # elimino city
-    print("***"*5 + " POISSON fourth ITERATION " + "***"*5)
-    formula = "outcome ~ home_team + away_team + continent + neutral"
-    model = smf.glm(formula=formula, data=df, family=sm.families.Poisson()).fit() # freq_weights, ricorda eventualmente di considerare..
-    print(model.summary())
+    # test_Linear_regr(df)
+    # test_Poisson(df)
+    test_Logit(df)
